0679864ae231cc60c2672f21e093035e4f3ba3b8
Aline Ferreira <alinfst@gmail.com>
2024-04-24 12:10:13 -0300

    add classes for UI

---
+++ app/src/main/java/br/com/aline/cryptocurrency/presentation/coin_detail/CoinListScreen.kt
@@ -0,0 +1,61 @@
+package br.com.aline.cryptocurrency.presentation.coin_detail
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import br.com.aline.cryptocurrency.presentation.Screen
+import br.com.aline.cryptocurrency.presentation.coin_detail.components.CoinListItem
+import br.com.aline.cryptocurrency.presentation.coin_list.CoinListViewModel
+import java.nio.file.WatchEvent
+
+@Composable
+fun CoinListScreen(
+    navController: NavController,
+    viewModel: CoinListViewModel = hiltViewModel()
+) {
+    val state = viewModel.state.value
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(state.coins) { coin ->
+                CoinListItem(
+                    coin = coin,
+                    onItemClick = {
+                        navController.navigate(Screen.CoinDetailcreen.route + "/${coin.id}")
+                    }
+                )
+            }
+
+        }
+        if (state.error.isNotBlank()) {
+            Text(
+                text = state.error,
+                color = MaterialTheme.colorScheme.error,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .align(Alignment.Center)
+
+            )
+        }
+        if (state.isLoading){
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+    }
+}
